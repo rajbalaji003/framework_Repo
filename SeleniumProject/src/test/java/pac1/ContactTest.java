ee282f2d78fa44e6b758c84c5ac70108d9a276e9
rajbalaji003 <rajbalaji003@gmail.com>
2025-03-17 15:41:25 +0530

    added new test

--- SeleniumProject/src/test/java/pac1/ContactTest.java
+++ SeleniumProject/src/test/java/pac1/ContactTest.java
@@ -27,7 +27,12 @@ public class ContactTest {
 	
 	public void modifycontactTest() {
 		System.out.println("execute modifycontactTest");
+}
 		
+@Test 
+		
+		public void deletecontactTest() {
+			System.out.println("execute deletecontactTest");
 	}
 
 	
